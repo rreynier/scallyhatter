f0db2a865ace20bae6aeb7101999f57cadc1d521
Roeland Reyniers <roeland@bythepixel.com>
2016-10-06 22:58:42 -0600

    Remove deprecated tracking

--- application/views/templates/main.php
+++ application/views/templates/main.php
@@ -89,27 +89,5 @@
             </div>
         </div>
     
-    </body>
-
-<!-- Woopra Code Start -->
-<script type="text/javascript" src="//static.woopra.com/js/woopra.v2.js"></script>
-<script type="text/javascript">
-woopraTracker.track();
-</script>
-<!-- Woopra Code End -->
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-18822419-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-    
+    </body>    
 </html>
